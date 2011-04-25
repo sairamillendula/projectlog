31ee2b66c7e6b0eaf6a77839f63d1c74a0050914
Andrés Mejía <andmej@gmail.com>
2011-04-25 09:56:46 -0500

    Contact deletion is working. Very subtle bug!

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,11 @@
 class ContactsController < ApplicationController
   before_filter :authenticate_user!
   #before_filter :load_customer
-  
+
   def index
     @customer = Customer.find(params[:customer_id])
     @contacts = @customer.contacts.all
-    
+
     respond_to do |format|
       format.html # index.html.erb
     end
@@ -44,12 +44,13 @@ class ContactsController < ApplicationController
         format.js 
       else
         respond_to do |format|
-        format.html { redirect_to @customer, :alert => 'Unable to add contact' }
-        format.js { render 'fail_create.js.erb' }
+          format.html { redirect_to @customer, :alert => 'Unable to add contact' }
+          format.js { render 'fail_create.js.erb' }
+        end
       end
     end
   end
-  
+
   def update
     @customer = Customer.find(params[:customer_id])
     @contact = @customer.contacts.find(params[:id])
@@ -64,14 +65,12 @@ class ContactsController < ApplicationController
   end
 
   def destroy
-    #@customer = Customer.find(params[:customer_id])
-    @contact = Contact.find(params[:id])
+    @customer = Customer.find(params[:customer_id])
+    @contact = @customer.contacts.find(params[:id])
     @contact.destroy
 
     respond_to do |format|
-      format.html { redirect_to(customers_url, :notice => 'Contact deleted') }
+      format.html { redirect_to(@customer, :notice => 'Contact deleted') }
     end
   end
-  
-end
 end
