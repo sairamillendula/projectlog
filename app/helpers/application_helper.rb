b88cad372579e10116c545156172d2359c2c132a
olimart <my.olivier.simart@gmail.com>
2011-05-11 00:49:07 -0400

    CSS fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def sortable(column, url_options = {}, title = nil)
     title ||= column.titleize
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    css_class = column == sort_column ? "sort #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, params.merge(:sort => column, :direction => direction, :page => nil).merge(url_options), {:class => css_class}
   end  
