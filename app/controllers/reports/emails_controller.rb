8b78b3d49d66cc23b6c667404c783034be3d7ee8
Andrés Mejía <andmej@gmail.com>
2011-05-29 12:58:49 -0500

    Passing the report link from the controller since the model has no access to the url helper methods

--- app/controllers/reports/emails_controller.rb
+++ app/controllers/reports/emails_controller.rb
@@ -1,7 +1,7 @@
 class Reports::EmailsController < ApplicationController
   def new
     @report = Report.find_by_slug!(params[:report_id])
-    @email = Reports::Email.new(:from => current_user.name_with_email, :to => current_user.connections.collect { |c| c.name_with_email }.compact.join(", "), :body => "Hello", :subject => "I want to share this report with you")
+    @email = Reports::Email.new(:to => current_user.connections.collect { |c| c.name_with_email }.compact.join(", "), :body => "Hello", :subject => "I want to share this report with you", :report_link => shared_report_url(@report))
     respond_to do |format|
       format.html
       format.js
