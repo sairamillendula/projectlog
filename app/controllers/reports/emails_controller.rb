6822a018259e6bda3c8d537e4392921057cb5781
Andrés Mejía <andmej@gmail.com>
2011-05-29 01:43:54 -0500

    Adding basic AJAX form for emails. Needs a lot of styling! Next step is adding the Mailer so the message is truly sent.

--- app/controllers/reports/emails_controller.rb
+++ app/controllers/reports/emails_controller.rb
@@ -1,7 +1,14 @@
 class Reports::EmailsController < ApplicationController
   def new
+    @report = Report.find_by_slug!(params[:report_id])
     respond_to do |format|
       format.html
+      format.js
+    end
+  end
+  
+  def create
+    respond_to do |format|
       format.js
     end
   end
