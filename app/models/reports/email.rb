d1df8398a834198ec2b9172f0080afc3224f56b3
Andrés Mejía <andmej@gmail.com>
2011-05-29 17:13:29 -0500

    Adding regular expressions for validation purposes

--- app/models/reports/email.rb
+++ app/models/reports/email.rb
@@ -15,5 +15,21 @@ class Reports::Email
   
   def persisted?
     false
-  end  
+  end
+  
+  def self.list_of_names_with_emails_regexp
+    # Matches a list of names with emails. For example:
+    # 'Andrés Mejía <andmej@gmail.com>,   Olivier SIMART <olivier.simart@gmail.com>, olivier@koopon.ca'
+    /\A(#{name_with_email_regexp},\s*)*#{name_with_email_regexp}\z/
+  end
+  
+  def self.email_regexp
+    # Matches an email address. (Stolen from Devise)
+    /([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})/i
+  end
+  
+  def self.name_with_email_regexp
+    # Matches 'Andrés Mejía <andmej@gmail.com>' or 'andmej@gmail.com', but doesn't match 'Andrés Mejía'
+    /(((\S+\s*)+<#{email_regexp}>)|(#{email_regexp}))/    
+  end
 end
