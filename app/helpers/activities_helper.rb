eada228866ae83cb287af8d7f95dba31d544247f
Olivier <olivier@yafoy.com>
2011-07-02 19:22:19 -0400

    partial number localization & added activities to days method

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,8 @@
 module ActivitiesHelper
+
+def activities_in_days(total_hours)
+  @activities_in_daysa = pluralize( number_with_precision((total_hours) / current_user.profile.hours_per_day, :precision => 2),
+                        "day")
+end
+
 end
