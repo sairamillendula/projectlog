0a9ebf0940e545132fcd7bc6174a34afeb61d7bc
Olivier <olivier@yafoy.com>
2011-07-05 18:52:44 -0400

    Fixed activities_to_days when nil

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,8 +1,8 @@
 module ActivitiesHelper
 
   def activities_to_days(total_hours)
-    @activities_to_days = pluralize( number_with_precision((total_hours) / current_user.profile.hours_per_day, :precision => 2 ),
-                          "day")
+    @activities_to_days = "[" + pluralize( number_with_precision((total_hours) / current_user.profile.hours_per_day, :precision => 2 ),
+                          "day") + "]" unless (current_user.profile.hours_per_day.nil?)
   end
 
 end
