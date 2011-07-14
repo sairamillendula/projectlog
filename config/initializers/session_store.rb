a9b1c1c4bc9712a4e685889994523b58b26827bf
Andrés Mejía <andmej@gmail.com>
2011-07-14 15:49:19 -0500

    Updating to Rails 3.1.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Projectlog::Application.config.session_store :cookie_store, :key => '_projectlog_session'
+Projectlog::Application.config.session_store :cookie_store, key: '_projectlog_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
