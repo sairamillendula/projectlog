26dcc63b3e74e8f01a20883355c0f6dffe9f914a
Olivier <olivier@yafoy.com>
2011-07-16 15:48:02 -0400

    Favicon

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+# Added to avoid 'could not parse YAML at line XX column XX'
+require 'yaml'
+YAML::ENGINE.yamler= 'syck'
