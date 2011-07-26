34b980c062ab27acbffd57376efcccc4aab14f38
Andrés Mejía <andmej@gmail.com>
2011-07-26 17:32:52 -0500

    Validating that start_date is smaller than end_date on reports. Closes #32. Total time: 10 minutes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,12 +19,4 @@ $(function() {
   $("input.date").live('click', function() {
     $(this).datepicker({showOn:'focus', dateFormat : "yy-mm-dd"}).focus();
   });
-    
-  $("#quick_search_start_date").change(function(){
-    var start_date = new Date(Date.parse($(this).val()));
-    var end_date = new Date(start_date.getFullYear(), start_date.getMonth() + 1, 1); // First day of the next month
-    end_date = new Date(end_date - 1); // Last second of the current month
-    $("#quick_search_end_date").val(end_date.getFullYear() + "-" + (end_date.getMonth() + 1) + "-" + end_date.getDate()); // Convert the date to 'YYYY-MM-DD' format
-    return false;
-  });
 });
--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -5,9 +5,16 @@ $(function(){
     e.preventDefault();
   });
   
-  console.log(window.location); 
   if (document.URL.indexOf("form=advanced") >= 0) {
        // Toggle to advanced form right away.
        $("#toggle_search_form").click();
   }
+  
+  $("#quick_search_start_date").change(function(){
+    var start_date = new Date(Date.parse($(this).val()));
+    var end_date = new Date(start_date.getFullYear(), start_date.getMonth() + 1, 1); // First day of the next month
+    end_date = new Date(end_date - 1); // Last second of the current month
+    $("#quick_search_end_date").val(end_date.getFullYear() + "-" + (end_date.getMonth() + 1) + "-" + end_date.getDate()); // Convert the date to 'YYYY-MM-DD' format
+    return false;
+  });
 });
