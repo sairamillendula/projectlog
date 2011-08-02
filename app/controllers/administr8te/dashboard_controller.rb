1c721febd20c31bada18e6eadff18ab3f9aabe11
Olivier <olivier@yafoy.com>
2011-08-02 19:12:08 -0400

    Added more stats to Admin dashboard

--- app/controllers/administr8te/dashboard_controller.rb
+++ app/controllers/administr8te/dashboard_controller.rb
@@ -1,39 +1,22 @@
 class Administr8te::DashboardController < Administr8te::BaseController
   set_tab :admin_dashboard
   def show
-    data_table = GoogleVisualr::DataTable.new
     
-    @users = User.standard.all
-    
-    data_table.new_column('string', 'Year')
-    data_table.new_column('number', 'Sales')
-    data_table.new_column('number', 'Expenses')
-    data_table.add_rows( [
-        ['2004', 1000, 400],
-        ['2005', 1170, 460],
-        ['2006', 660, 1120],
-        ['2007', 1030, 540]
-      ])
-
-    options   = { width: 400, height: 240, title: 'Company Performance', hAxis: {title: 'Year', titleTextStyle: {color: '#FF0000'}} }
-    @users_chart = GoogleVisualr::Interactive::LineChart.new(data_table, options)
-    
-    
-    @s_count = User.standard.group("date(created_at)").count
+    @s_count = User.standard.group("date(created_at)").select("created_at, count(id) as count_users").size
     #@s_count = User.standard.count
 
     data_table = GoogleVisualr::DataTable.new
     # Add Column Headers 
-    data_table.new_column('string', 'Date' ) 
+    data_table.new_column('date', 'Day' ) 
     data_table.new_column('number', 'Total Count') 
 
     # Add Rows and Values 
     data_table.add_rows([ 
-      ['date', @s_count]
+      [Date.parse('2011-08-02'), @s_count]
     ])
 
     option = { width: 880, height: 240, title: 'Signup per day' }
-    @chart2 = GoogleVisualr::Interactive::LineChart.new(data_table, option)
+    @users_chart = GoogleVisualr::Interactive::LineChart.new(data_table, option)
     
   end
 
--- app/helpers/administr8te/dashboard_helper.rb
+++ app/helpers/administr8te/dashboard_helper.rb
@@ -5,7 +5,12 @@ module Administr8te::DashboardHelper
     #@active_users = User.where(:admin => false AND "last_sign_in_at < ?, 3.months.ago" ).includes(:activities).size
     #@active_users = User.standard.where("last_sign_in_at > ?", 3.months.ago).size
     #@active_users_with_activities = @active_users.activities_i.any?
-    @active_users = User.where(:admin => false).joins(:projects).group('"projects"."user_id"').having('COUNT(projects.id) > 5').size
+    @active_users = User.standard.joins(:projects).group('"projects"."user_id"').having('COUNT(projects.id) > 1').size
+  end
+  
+  def average_projects_per_user
+    @projects_belonging_to_standard_users = User.standard.joins(:projects).all.size
+    @average_projects_per_user = @projects_belonging_to_standard_users.to_f / User.standard.size.to_f
   end
   
   def total_users_created(timeframe)
