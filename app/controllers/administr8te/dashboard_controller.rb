e225dbdf8a8b7ae09cf4d9dc99cb9481d83afa40
Olivier <olivier@yafoy.com>
2011-08-02 11:38:45 -0400

    added Chart test under admin dashboard

--- app/controllers/administr8te/dashboard_controller.rb
+++ app/controllers/administr8te/dashboard_controller.rb
@@ -1,22 +1,22 @@
 class Administr8te::DashboardController < Administr8te::BaseController
   set_tab :admin_dashboard
   def show
-  end
-  
-  def area_chart
     data_table = GoogleVisualr::DataTable.new
+    
+    @users = User.standard.all
+    
     data_table.new_column('string', 'Year')
     data_table.new_column('number', 'Sales')
     data_table.new_column('number', 'Expenses')
     data_table.add_rows( [
-      ['2004', 1000, 400],
-      ['2005', 1170, 460],
-      ['2006', 660, 1120],
-      ['2007', 1030, 540]
-    ])
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
+      ])
 
     options   = { width: 400, height: 240, title: 'Company Performance', hAxis: {title: 'Year', titleTextStyle: {color: '#FF0000'}} }
-    @chart = GoogleVisualr::Interactive::AreaChart.new(data_table, options)
+    @users_chart = GoogleVisualr::Interactive::LineChart.new(data_table, options)
   end
-  
+
 end
