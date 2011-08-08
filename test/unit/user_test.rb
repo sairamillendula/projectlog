49c3bd3f6d23317c8bb12ea044525016e79d8dd4
Andrés Mejía <andmej@gmail.com>
2011-08-07 20:54:57 -0500

    Making sure an user is assigned the free plan after creation. Related to #18. Total time: 10 minutes.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have the free plan automatically asigned after creation" do
+    u = User.create!(:email => "asdf@adsf.com", :password => "123456")
+    assert u.plan, "user doesn't have a plan"
+    assert_equal u.plan, Plan.find_by_name!("Free")
+    assert User.with_free_plan.all.include?(u)
+  end
 end
