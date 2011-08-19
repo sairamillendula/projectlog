0447311c67171ce5c29dd736a50a35f494974519
Olivier <olivier@yafoy.com>
2011-08-19 12:04:08 -0400

    Reworked Site Announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,7 +2,8 @@ class Announcement < ActiveRecord::Base
   validates_presence_of :message, :starts_at, :ends_at
   serialize :hidden_by_user_ids, Array
   
-  scope :current_announcements, lambda { where("starts_at <= ? AND ends_at >= ?", Time.now, Time.now) }
+  default_scope :order => 'ends_at DESC'
+  scope :current_announcements, lambda { where("starts_at <= ? AND ends_at >= ?", Time.zone.now, Time.zone.now) }
   
   def hidden_by?(user)
     hidden_by_user_ids.include?(user.id)
