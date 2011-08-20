162fb02907fbc7c24591f16080dd57c1d21118c0
Olivier <olivier@yafoy.com>
2011-08-19 21:11:33 -0400

    Report unit test + fixture

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,8 +1,38 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should create report" do
+    report = Report.new
+    report.user_id = users(:one)
+    report.start_date = '2011-04-26'
+    report.end_date = '2012-04-26'
+    assert_not_nil(report.user_id)
+    assert report.save
+    assert_not_nil report.slug
+  end
+  
+  test "mandatory fields should be filled in" do
+    report = Report.new
+    assert !report.valid?
+    assert report.errors[:start_date].any?
+    assert report.errors[:end_date].any?
+    assert report.errors[:user_id].any?
+    assert_equal ["can't be blank"], report.errors[:start_date]
+    assert_equal ["can't be blank"], report.errors[:end_date]
+    assert_equal ["can't be blank"], report.errors[:user_id]
+    assert !report.save
+  end
+  
+  test "end date must be greater than start date" do
+    report = Report.new
+    report.user_id = users(:one)
+    report.start_date = '2011-04-26'
+    report.end_date = '2010-04-26'
+    assert !report.valid?
+    assert report.errors[:start_date].any?
+    assert_equal ["must be smaller than end date"], report.errors[:start_date]
+    assert !report.save
+  end
+  
 end
