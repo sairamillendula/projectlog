48b6ca559aa72392d7da3a6df7b80f14ccc29578
Olivier <olivier@yafoy.com>
2011-08-21 15:52:46 -0400

    Report/email unit test

--- test/unit/reports/email_test.rb
+++ test/unit/reports/email_test.rb
@@ -2,8 +2,35 @@ require 'test_helper'
 
 class Reports::EmailTest < ActiveSupport::TestCase
   
-  test "" do
-    
+  test "should send email" do
+    report = Report.find_by_slug!("thisisthereportslug").id
+    email = Reports::Email.new
+    email.from = "notification@getprojectlog.com"
+    email.to = "user@gmail.com"
+    email.reply_to = "user@getprojectlog.com"
+    email.subject = "Sharing a report"
+    email.body = "Shared Timesheet"
+    email.report_link = "grtgrtgtrgtr"
+    assert email.deliver
+  end
+  
+  test "mandatory fields should be filled in" do
+    report = Report.find_by_slug!("thisisthereportslug").id
+    email = Reports::Email.new
+    assert !email.valid?
+    assert email.errors[:to].any?
+    assert email.errors[:from].any?
+    assert email.errors[:subject].any?
+    assert email.errors[:body].any?
+    assert email.errors[:report_link].any?
+    assert email.errors[:reply_to].any?
+    assert_equal ["can't be blank"], email.errors[:to]
+    assert_equal ["can't be blank"], email.errors[:from]
+    assert_equal ["can't be blank"], email.errors[:subject]
+    assert_equal ["can't be blank"], email.errors[:body]
+    assert_equal ["can't be blank"], email.errors[:report_link]
+    assert_equal ["can't be blank"], email.errors[:reply_to]
+    assert !email.deliver
   end
   
 end
