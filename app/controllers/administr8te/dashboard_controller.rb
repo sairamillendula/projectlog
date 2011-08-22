3d4a83fada6d16acb8ba6970528a16cadf2a83e5
Olivier <olivier@yafoy.com>
2011-08-22 13:13:48 -0400

    tried to build signup chart on admin dashboard. not working

--- app/controllers/administr8te/dashboard_controller.rb
+++ app/controllers/administr8te/dashboard_controller.rb
@@ -2,19 +2,20 @@ class Administr8te::DashboardController < Administr8te::BaseController
   set_tab :admin_dashboard
   def show
     
-    @s_count = User.standard.group("date(created_at)").select("created_at, count(id) as count_users").size
-    #@s_count = User.standard.count
+    s_count = User.standard.group("date(created_at)").select("created_at, count(id) as count_users").limit(30).size
 
     data_table = GoogleVisualr::DataTable.new
     # Add Column Headers 
-    data_table.new_column('date', 'Day' ) 
-    data_table.new_column('number', 'Total Count') 
+    #data_table.new_column('date', 'Day' ) 
+    #data_table.new_column('number', 'Total Count') 
 
     # Add Rows and Values 
-    data_table.add_rows([ 
-      [Date.parse('2011-08-02'), @s_count]
-    ])
-
+    data_table.new_column('string', 'Date')
+    data_table.new_column('number', 'Total')
+    data_table.add_rows(3)
+    data_table.set_cell(0, 0, 'Signup')
+    data_table.set_cell(0, 1, s_count)
+    
     option = { width: 880, height: 240, title: 'Signup per day' }
     @users_chart = GoogleVisualr::Interactive::LineChart.new(data_table, option)
     
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def billable_time_grouped_by_date_in_the_last_days(number_of_days = 7)
-    time_range = (Time.now.midnight - number_of_days.days)..Time.now.midnight
+    time_range = (Time.zone.now.midnight - number_of_days.days)..Time.zone.now.midnight
     projects = current_user.projects.billable.all
     Activity.where(:date => time_range).belonging_to_projects(projects).total_time_grouped_by_date
   end
