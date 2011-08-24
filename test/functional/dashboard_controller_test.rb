49ab96394fa5c759422c609f6a5d32ceb89d1bd2
Olivier <olivier@yafoy.com>
2011-08-24 17:09:47 -0400

    Changed dashboard controller functional test

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -3,10 +3,11 @@ include Devise::TestHelpers
 
 class DashboardControllerTest < ActionController::TestCase
   
-  test "should get show" do
+  test "should get show if no announcement" do
     sign_in users(:one)
     get :show
-    assert_response :redirect
+    assert_response :sucess
+    assert_template 'show'
   end
   
 end
