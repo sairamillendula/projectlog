48be14374a5981f569b87bf2303b3a7076546d9b
Andrés Mejía <andmej@gmail.com>
2011-08-24 01:55:13 -0500

    Adding 'Admin dashboard: render line chart for total user signup per day. Time range: last 30 days'. Closes #42.
    [t: 45 minutes]

--- app/controllers/administr8te/dashboard_controller.rb
+++ app/controllers/administr8te/dashboard_controller.rb
@@ -1,24 +1,5 @@
 class Administr8te::DashboardController < Administr8te::BaseController
   set_tab :admin_dashboard
   def show
-    
-    s_count = User.standard.group("date(created_at)").select("created_at, count(id) as count_users").limit(30).size
-
-    data_table = GoogleVisualr::DataTable.new
-    # Add Column Headers 
-    #data_table.new_column('date', 'Day' ) 
-    #data_table.new_column('number', 'Total Count') 
-
-    # Add Rows and Values 
-    data_table.new_column('string', 'Date')
-    data_table.new_column('number', 'Total')
-    data_table.add_rows(3)
-    data_table.set_cell(0, 0, 'Signup')
-    data_table.set_cell(0, 1, s_count)
-    
-    option = { width: 880, height: 240, title: 'Signup per day' }
-    @users_chart = GoogleVisualr::Interactive::LineChart.new(data_table, option)
-    
   end
-
 end
--- app/helpers/administr8te/dashboard_helper.rb
+++ app/helpers/administr8te/dashboard_helper.rb
@@ -30,4 +30,26 @@ module Administr8te::DashboardHelper
     @total_users_with_paid_plan_created = User.standard.with_paid_plan.where("users.created_at > ?", timeframe).size
   end
   
+  def signups_in_the_last_month_chart(container_dom_id)
+    users = User.standard.where("`users`.created_at > ?", 31.days.ago).select("created_at")
+    counts = {}
+    users.each do |user|
+      counts[user.created_at.to_date] ||= 0
+      counts[user.created_at.to_date] += 1
+    end
+
+    data_table = GoogleVisualr::DataTable.new
+    data_table.new_column('string', 'Day')
+    data_table.new_column('number', 'New users')
+    data_table.add_rows(31)
+    0.upto(30) do |i|
+      date = (30 - i).days.ago.to_date
+
+      data_table.set_cell(i, 0, l(date, :format => :short))
+      data_table.set_cell(i, 1, counts[date] || 0)
+    end
+
+    opts   = { :width => 880, :height => 240, :title => 'Sign ups in the last 31 days', :legend => 'none', :hAxis => { :textStyle => { :color => "black", :fontName => "Arial", :fontSize => 12 } } }
+    render_chart GoogleVisualr::Interactive::LineChart.new(data_table, opts), container_dom_id
+  end
 end
