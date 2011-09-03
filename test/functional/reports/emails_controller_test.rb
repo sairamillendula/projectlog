83cfbb6f3ec6a4dc019ae64fdb1a61aeb807a832
Andrés Mejía <andmej@gmail.com>
2011-09-03 18:40:52 -0500

    Implementing Reports/Emails controller test.
    Related to #55.
    
    [t: 15 minutes]

--- test/functional/reports/emails_controller_test.rb
+++ test/functional/reports/emails_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class Reports::EmailsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    sign_in users(:one)
+    @report = reports(:one)
+  end
+
+  test "should get new" do
+    get :new, :report_id => @report.to_param
+    assert_response :success
+    assert_template 'new'
+  end
+
+  test "should send report by email" do
+    Reports::Email.any_instance.expects(:deliver).returns(nil)
+    post :create, :format => :js, :report_id => @report.id, :reports_email => {
+      :to => "andmej@gmail.com",
+      :subject => "Interesting report",
+      :body => "Hi, check this out %{report_link}",
+      :report_link => "http://getprojectlog.reports/aaaaaaaaaaaaa"
+    }
+    assert_response :success
   end
 end
