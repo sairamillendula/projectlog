3775b6074e90937692b0148cd6200da8130a9cc2
Andrés Mejía <andmej@gmail.com>
2011-09-03 18:52:55 -0500

    Fixing tests for ReportMailer.
    Related to #55. [t: 10 minutes]

--- test/functional/reports_mailer_test.rb
+++ test/functional/reports_mailer_test.rb
@@ -1,23 +1,24 @@
 require 'test_helper'
-include Devise::TestHelpers
 
 class ReportsMailerTest < ActionMailer::TestCase
-  setup do
-    sign_in users(:one)
-    @report = reports(:one)
-  end
-  
-  test "should get new" do
-    get :new
-    assert_response :success
-    assert_template 'new'
-  end
+  test "report_shared_with_you" do
+    report = Report.find_by_slug!("thisisthereportslug").id
+    email = Reports::Email.new
+    email.from = "notification@getprojectlog.com"
+    email.to = "user@gmail.com"
+    email.reply_to = "user@getprojectlog.com"
+    email.subject = "Sharing a report"
+    email.body = "Shared Timesheet: %{report_link}"
+    email.report_link = "grtgrtgtrgtr"
 
-  test "should send report by email" do
-    assert_difference('Report.count') do
-      post :create, :report => @report.attributes
+    assert_difference('ActionMailer::Base.deliveries.count') do
+      email.deliver
     end
 
-    assert_redirected_to report_path(assigns(:report))
+    sent = ActionMailer::Base.deliveries.first
+    assert_equal ["user@gmail.com"], sent.to
+    assert_equal "Sharing a report", sent.subject
+    assert_equal ["user@getprojectlog.com"], sent.reply_to
+    assert sent.body =~ /Shared Timesheet: grtgrtgtrgtr/
   end
 end
