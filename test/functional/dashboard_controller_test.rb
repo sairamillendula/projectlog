876c580b6e5a1cfdb075e509cf7434ecb9259c54
Andrés Mejía <andmej@gmail.com>
2011-09-08 16:06:47 -0500

    Fixing functional tests for DashboardController.
    Related to #55. [t: 5 minutes]

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -1,12 +1,11 @@
 require 'test_helper'
-include Devise::TestHelpers
 
 class DashboardControllerTest < ActionController::TestCase
   
   test "should get show if no announcement" do
     sign_in users(:one)
     get :show
-    assert_response :sucess
+    assert_response :success
     assert_template 'show'
   end
   
