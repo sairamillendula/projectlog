a8be48f59cf2441b747a802b6cc50f318320fd2f
Andrés Mejía <andmej@gmail.com>
2011-09-08 16:14:50 -0500

    Fixing functional tests for Administr8te::LogsController.
    Related to #55. [t: 5 minutes]

--- test/functional/administr8te/logs_controller_test.rb
+++ test/functional/administr8te/logs_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class Administr8te::LogsControllerTest < ActionController::TestCase
+  setup do
+    sign_in users(:one)
+  end
+
   test "should get show" do
     get :show
     assert_response :success
   end
-
 end
