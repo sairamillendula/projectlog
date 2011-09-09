058f1685c40b52ef73354c18e64fb2b3906a32bc
Olivier <olivier.simart@gmail.com>
2011-09-09 05:13:12 -0400

    Added administr8te_settings func. test. Update action needs to be added later on

--- test/functional/administr8te/settings_controller_test.rb
+++ test/functional/administr8te/settings_controller_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class Administr8te::SettingsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should not get edit if not admin" do
+    sign_in users(:two)
+    get :edit
+    assert_response :redirect
   end
+  
+  setup do
+    sign_in users(:one)
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+    assert_template 'edit'
+  end
+  
+  test "should update administr8te_settings" do
+    #put :update
+    #assert_response :success
+    #assert_redirected_to edit_administr8te_settings_path
+  end
+  
 end
