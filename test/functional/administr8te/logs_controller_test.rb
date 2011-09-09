423bc5fc05bffbd768b95e90c931aac6505b88a8
Olivier <olivier.simart@gmail.com>
2011-09-09 05:03:00 -0400

    Reworked administr8te_logs func. test

--- test/functional/administr8te/logs_controller_test.rb
+++ test/functional/administr8te/logs_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class Administr8te::LogsControllerTest < ActionController::TestCase
+  test "should not get show if not admin" do
+    sign_in users(:two)
+    get :show
+    assert_response :redirect
+  end
+  
   setup do
     sign_in users(:one)
   end
@@ -8,5 +14,6 @@ class Administr8te::LogsControllerTest < ActionController::TestCase
   test "should get show" do
     get :show
     assert_response :success
+    assert_template 'show'
   end
 end
