5fe352c1ff91c9582550bef1697a2c303adb4945
Olivier <olivier.simart@gmail.com>
2011-09-09 05:00:58 -0400

    Added administr8te_dashboard func. test

--- test/functional/administr8te/dashboard_controller_test.rb
+++ test/functional/administr8te/dashboard_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class Administr8te::DashboardControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should not get show if not admin" do
+    sign_in users(:two)
+    get :show
+    assert_response :redirect
   end
+  
+  setup do
+    sign_in users(:one)
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+    assert_template 'show'
+  end
+  
 end
