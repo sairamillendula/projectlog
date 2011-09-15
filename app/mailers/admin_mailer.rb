4937ba2de6774529accd53a2c921fa69f5d07572
Olivier <olivier.simart@gmail.com>
2011-09-15 15:36:32 -0400

    Added AdminMailer Func. tests

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,8 +1,8 @@
 class AdminMailer < ActionMailer::Base
   default from: "no-reply@getprojectlog.com"
   
-  def new_user_registered(new_user)
-    @user = new_user
+  def new_user_registered(user)
+    @user = user
     mail(:to => "app@getprojectlog.com", :subject => "[Projectlog] New user joined")
   end
   
--- test/functional/admin_mailer_test.rb
+++ test/functional/admin_mailer_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class AdminMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should send email to Admin for every new signup" do
+      user = User.new
+      user.email = 'test@email.com'
+      user.password = '123456'
+      user.first_name = 'Jean'
+      user.last_name = 'Pierre'
+      assert user.save
+      
+      message = AdminMailer.new_user_registered(user).deliver
+      assert_equal "[Projectlog] New user joined", message.subject
+      assert_equal ["app@getprojectlog.com"], message.to
+  end
+  
+  test "should send email to Admin when :admin attribute changes" do
+      # First new admin joins
+      user = users(:one)
+      user.admin = true
+      assert user.save
+      
+      message = AdminMailer.new_system_administrator(user).deliver
+      assert_equal "[Projectlog] Administrator change", message.subject
+      assert_equal ["app@getprojectlog.com"], message.to
+      
+      # Repeat if admin becomes standard user again
+      user.admin = false
+      assert user.save
+      
+      message = AdminMailer.new_system_administrator(user).deliver
+      assert_equal "[Projectlog] Administrator change", message.subject
+      assert_equal ["app@getprojectlog.com"], message.to
+  end
+  
 end
