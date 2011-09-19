fb27ebbdd1c9df90c96f260c12b9abf8facd7cbb
Olivier <olivier.simart@gmail.com>
2011-09-18 22:23:55 -0400

    Changed production environment for projectlogapp.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Projectlog::Application.configure do
   config.assets.digest = true
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
@@ -61,15 +61,15 @@ Projectlog::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'getprojectlog.com' }
+  config.action_mailer.default_url_options = { :host => 'projectlogapp.com' }
   config.action_mailer.raise_delivery_errors = true
   
   # Getprojectlog Email server setup
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {  
-    :address              => "mail.getprojectlog.com",  
+    :address              => "mail.projectlogapp.com",  
     :port                 => 587,
-    :user_name            => "no-reply+getprojectlog.com",  
+    :user_name            => "notifications+projectlogapp.com",  
     :password             => "10eytd10",  
     :authentication       => "plain",  
     :enable_starttls_auto => false  
