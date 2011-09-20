3608df24d2ccf1e0e92ac932718d78fcf8540a25
Andrés Mejía <andmej@gmail.com>
2011-09-19 23:06:05 -0500

    Reordering production.rb.
    Looks nicer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,15 +8,6 @@ Projectlog::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Specify the default JavaScript compressor
-  config.assets.js_compressor  = :uglifier
-
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
   config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
@@ -38,6 +29,12 @@ Projectlog::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Specify the default JavaScript compressor
+  config.assets.js_compressor  = :uglifier
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
