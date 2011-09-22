4cf61f4c2a4a16b2fad87e4c861d924a3447b7f2
Olivier <olivier.simart@gmail.com>
2011-09-22 01:33:30 -0400

    Trigger Mailchimp API on create, update & destroy

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,10 +1,13 @@
 class UserObserver < ActiveRecord::Observer
+  def h
+    h = Hominid::API.new(api_key, {:secure => true, :timeout => 60})
+  end
+  
   def after_create(user)
     AdminMailer.new_user_registered(user).deliver
     WelcomeMailer.welcome_email(user).deliver
     
     # Sending user to 3rd party
-    h = Hominid::API.new(api_key, {:secure => true, :timeout => 60})
     h.list_subscribe(list_id, user.email, {'FNAME' => user.first_name, 'LNAME' => user.last_name}, 'html', false, true, true, false)
   end
   
@@ -12,8 +15,17 @@ class UserObserver < ActiveRecord::Observer
     if user.admin_changed?
       AdminMailer.new_system_administrator(user).deliver
     end
+    if user.email_changed? or user.first_name_changed? or user.last_name_changed?
+      #listUpdateMember(string apikey, string id, string email_address, array merge_vars, string email_type, boolean replace_interests)
+      h.list_update_member(list_id, user.email, {'FNAME' => user.first_name, 'LNAME' => user.last_name}, 'html', false)
+    end
   end
   
+  def after_destroy(user)
+    #listUnsubscribe(string apikey, string id, string email_address, boolean delete_member, boolean send_goodbye, boolean send_notify)
+    # Don't delete user in MC just unsubscribe
+    h.list_unsubscribe(list_id, user.email, false, false, false)
+  end
   
   private
   
