215fa4839bfc8597ca2f46321060000dce29d1bb
Olivier <olivier.simart@gmail.com>
2011-09-22 14:36:25 -0400

    Added unit testing for emailing

--- app/models/emailing.rb
+++ app/models/emailing.rb
@@ -1,4 +1,6 @@
 class Emailing < ActiveRecord::Base
   attr_accessible :active, :description, :api_key, :list_key
   scope :active, where(:active => true)
+  
+  validates_presence_of :active, :description, :api_key
 end
--- test/unit/emailing_test.rb
+++ test/unit/emailing_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class EmailingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "should create emailing" do
+    emailing = Emailing.new
+    emailing.description = emailings(:one)
+    emailing.api_key = emailings(:one)
+    assert emailing.save
+  end
+  
+  test "should not save without status, description and api_key " do
+    emailing = Emailing.new
+    assert !emailing.valid?
+    assert emailing.errors[:description].any?
+    assert emailing.errors[:api_key].any?
+    assert_equal ["can't be blank"], emailing.errors[:description]
+    assert_equal ["can't be blank"], emailing.errors[:api_key]
+    assert !emailing.save
+  end
+  
+  test "should find emailing" do
+    emailing_id = emailings(:one).id
+    assert_nothing_raised { Emailing.find(emailing_id) }
+  end
+
+  test "should update emailing" do
+    emailing = emailings(:one)
+    assert emailing.update_attributes(:list_key => '23dxdT6fg8g')
+  end
+  
+  test "should destroy emailing" do
+    emailing = emailings(:one)
+    emailing.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Emailing.find(emailing.id) }
+  end
+  
 end
