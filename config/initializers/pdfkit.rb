bae56b3d6caf69397ef1e0c4ac13d2ecdf8a7260
arpadlukacs <arpad.lukacs@gmail.com>
2011-12-06 09:54:30 +0100

    PDF fix

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,8 +1,11 @@
 PDFKit.configure do |config|
-  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf').to_s if Rails.env == 'production'
-  config.default_options = {
-  :print_media_type => true,
-  :encoding => 'UTF-8'
-  }
-  #config.root_url = "http://localhost/" # Use only if your external hostname is unavailable on the server.
+  config.default_options = { page_size: 'A4', print_media_type: true, :encoding => 'UTF-8' }
+  if RUBY_PLATFORM =~ /linux/
+    wkhtmltopdf_executable = 'wkhtmltopdf-amd64'
+  elsif RUBY_PLATFORM =~ /darwin/
+    wkhtmltopdf_executable = 'wkhtmltopdf-osx'
+  else
+    raise "Unsupported. Must be running linux or intel-based Mac OS."
+  end
+  config.wkhtmltopdf = Rails.root.join('vendor', 'bin', wkhtmltopdf_executable).to_s
 end
