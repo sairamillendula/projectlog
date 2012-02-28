976c731f4b224dfb7094e1a3d051164d3d44fcb3
Olivier <olivier.simart@gmail.com>
2012-02-28 11:38:12 -0500

    removed extra quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Projectlog::Application.configure do
   # :user_name      => "<%= mail_server_username %>",
   # :password       => "<%= mail_server_password %>",
   #:domain         => "<%= mail_server_domain %>",
-  # :enable_starttls_auto => "<%= mail_server_starttls %>"
+  # :enable_starttls_auto => <%= mail_server_starttls %>
   # }
   
   ActionMailer::Base.smtp_settings = {
