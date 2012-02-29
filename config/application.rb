43124c4e500acb503861447096b0245b84837e4e
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-02-29 11:57:20 +0700

    update config

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Projectlog
     config.assets.enabled = true
     
     # Version of your assets, change this if you want to expire all your assets
-    # config.assets.version = '1.0'
+    config.assets.version = '1.0'
     #config.middleware.use "PDFKit::Middleware", :print_media_type => true
   end
 end
