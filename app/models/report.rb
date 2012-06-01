2a43fb54b81f78ee74c14a6b892acde195d48797
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-06-01 22:20:24 +0700

    Issue #94: fix report bug

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,7 +4,7 @@ class Report < ActiveRecord::Base
   belongs_to :user
   
   attr_accessible :project_id, :start_date, :end_date, :slug, :approved, :approved_at, :approved_ip, :user_id
-  validates_presence_of :start_date, :end_date, :user_id, :slug
+  validates_presence_of :user_id, :slug
   validate :start_date_must_be_smaller_than_end_date
   
   before_validation :generate_random_slug, :on => :create
@@ -14,7 +14,10 @@ class Report < ActiveRecord::Base
   end
   
   def activities
-    user.activities.after(start_date).before(end_date).belonging_to_projects(projects)
+    scoped = user.activities
+    scoped = scoped.after(start_date) if start_date.present?
+    scoped = scoped.before(end_date) if end_date.present?
+    scoped.belonging_to_projects(projects)
   end
   
   def projects
@@ -31,7 +34,10 @@ class Report < ActiveRecord::Base
   end
     
   def description
-    "#{project_title} from #{I18n.l start_date, :format => :long} to #{I18n.l end_date, :format => :long}"
+    arr = [project_title]
+    arr << "from #{I18n.l start_date, :format => :long}" if start_date.present?
+    arr << "to #{I18n.l end_date, :format => :long}" if end_date.present?
+    arr.join(" ")
   end
   
   def approve!(ip_address)
@@ -47,8 +53,12 @@ class Report < ActiveRecord::Base
   end
   
   def start_date_must_be_smaller_than_end_date
-    if start_date.present? && end_date.present? && start_date > end_date
-      errors.add(:start_date, "must be smaller than end date")
+    if start_date.blank? && end_date.blank?
+      errors.add(:base, "Start date or end date must be specified")
+    else
+      if start_date.present? && end_date.present? && start_date > end_date
+        errors.add(:start_date, "must be smaller than end date")
+      end
     end
   end
 
