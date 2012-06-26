ac0154962944cdfc0f3683d4b267de4ce7ccef2f
Hoang Nghiem <hoangnghiem1711@gmail.com>
2012-06-26 12:23:21 +0700

    update deploy branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:andmej/#{application}.git"
-set :branch, "linode"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
