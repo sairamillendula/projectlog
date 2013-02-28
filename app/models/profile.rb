6fc7d93f39a602cfebb157e352108d080b2b0d8d
Olivier <olivier.simart@gmail.com>
2013-02-28 15:05:31 -0500

    Added hours to days in report pdf

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -13,11 +13,10 @@ class Profile < ActiveRecord::Base
 
   attr_accessor :remove_logo
   attr_accessible :company, :address1, :address2, :city, :province, :postal_code, :country, :phone_number, :localization, :hours_per_day,
-                  :website, :user_id, :user_id, :tax1, :tax2, :tax1_label, :tax2_label, :invoice_signature, :compound, :fiscal_year, :logo, :remove_logo
+                  :website, :user_id, :user_id, :tax1, :tax2, :tax1_label, :tax2_label, :invoice_signature, :compound, :fiscal_year, :logo,
+                  :remove_logo
 
-
-
-  validates_numericality_of :hours_per_day, :on => :update, :allow_blank => true, :greater_than_or_equal_to => 2, :less_than_or_equal_to => 10,
+  validates_numericality_of :hours_per_day, on: :update, allow_blank: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 10,
                             :message => "must be numeric (no comma) and between 2 and 10."
 
   validates_attachment_content_type :logo, :content_type => IMAGE_TYPES, :message => 'must be an image (jpeg, png or gif)'
