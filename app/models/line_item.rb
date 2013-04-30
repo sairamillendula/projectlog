cedbc5ffdf2634eaffe799d2fc24b6d63bb1c7f7
Olivier <olivier.simart@gmail.com>
2013-04-30 19:37:56 -0400

    Allow negative quantit in line_item

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,8 +4,8 @@ class LineItem < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :quantity
   validates_presence_of :price
-  validates_numericality_of :quantity, :greater_than_or_equal_to => 0
-  validates_numericality_of :price, :greater_than_or_equal_to => 0
+  validates_numericality_of :quantity
+  validates_numericality_of :price, greater_than_or_equal_to: 0
   
   # subtotal excludes taxes
   def subtotal
