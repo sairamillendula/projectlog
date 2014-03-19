2106e9dfd731c8d4b0a546a95bab6ddf33663825
Olivier <olivier.simart@gmail.com>
2014-03-19 18:43:43 -0400

    Transaction to CSV returns proper date format

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,17 +1,17 @@
 class Transaction < ActiveRecord::Base
   include Taxable
-  
+
   belongs_to :project
   belongs_to :category
   belongs_to :user
-  
+
   attr_accessible :expense, :date, :total, :tax1, :tax2, :tax1_label, :tax2_label, :tax1_amount, :tax2_amount, :compound, :receipt, :note,
                   :recurring, :project_id, :category_id, :category_name, :user_id
   validates_presence_of :date, :total, :note
   validates_numericality_of :total
   validates_numericality_of :tax1, :tax2, :allow_blank => true
   validate :validate_limit, :on => :create
-  
+
   scope :expenses, where(expense: true)
   scope :incomes, where(expense: false)
   scope :by_category, lambda {|category_id| where(category_id: category_id)}
@@ -19,11 +19,11 @@ class Transaction < ActiveRecord::Base
   scope :to_date, lambda {|date| where("transactions.date <= ?", date)}
   scope :by_keyword, lambda {|keyword| where('(transactions.note LIKE ?) OR (transactions.total = ?)', "%#{keyword}%", keyword.gsub(/,/, '.').to_f)}
   scope :by_period, lambda {|date_range| where(:date => date_range)}
-  
+
   def subtotal
     (total - (try(:tax1) || 0) - (try(:tax2) || 0)).round(2)
   end
-  
+
   def self.search(search)
     if search
       where('"transactions"."note" LIKE ?', "%#{search}%")
@@ -31,11 +31,11 @@ class Transaction < ActiveRecord::Base
       scoped
     end
   end
-  
+
   def category_name
     category.try(:name)
   end
-  
+
   def category_name=(name)
     if name.blank?
       self.category = nil
@@ -44,25 +44,25 @@ class Transaction < ActiveRecord::Base
       self.category = current_user.categories.find_or_create_by_name(name)
     end
   end
-  
+
   # CLASS METHODS
   # =============
   class << self
-    
+
     # return total incomes of given transactions
     # assumes that transactions already loaded
     def total_incomes(transactions)
       transactions.select{|t| !t.expense }.inject(0) { |sum, p| sum + p.total }
     end
-    
+
     # return total expenses of given transactions
     # assumes that transactions already loaded
     def total_expenses(transactions)
       transactions.select{|t| t.expense }.inject(0) { |sum, p| sum + p.total }
     end
-    
+
     def fiscal_periods(from, to)
-      
+
       if fiscal_year.blank?
         from = Date.new(this_year - 1, 1, 1)
         to = Date.new(this_year, 1, 1)
@@ -78,14 +78,14 @@ class Transaction < ActiveRecord::Base
       CSV.generate(options) do |csv|
         csv << ['Date','Type','Total','Tax1','Tax2','Description','Category','Project']
         transactions.each do |transaction|
-          csv << [I18n.localize(transaction.date), transaction.expense? ? 'Expense' : 'Income', transaction.total, transaction.tax1, transaction.tax2, transaction.note, transaction.category.present? ? transaction.category.name : nil , transaction.project.present? ? transaction.project.title : nil]
+          csv << [transaction.date.to_date, transaction.expense? ? 'Expense' : 'Income', transaction.total, transaction.tax1, transaction.tax2, transaction.note, transaction.category.present? ? transaction.category.name : nil , transaction.project.present? ? transaction.project.title : nil]
         end
       end
     end
   end
-  
+
   private
-  
+
   def validate_limit
     user = User.find(self['user_id'])
     unless user.admin?
@@ -99,5 +99,5 @@ class Transaction < ActiveRecord::Base
       end
     end
   end
-  
+
 end
